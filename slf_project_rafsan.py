268c9c6653ea39ca91e2bc225f7075dee5bd5206
Rafsan Bhuiyan <48545950+rafsanbhuiyan@users.noreply.github.com>
2022-03-17 23:53:05 -1000

    Creating a new column with company name

--- slf_project_rafsan.py
+++ slf_project_rafsan.py
@@ -2,6 +2,7 @@ from numpy import disp
 import pyodbc
 import mysql.connector
 import pandas as pd
+from sympy import true
 
 server = 'rafsanserve.database.windows.net,1433'
 database = 'sfl_database'
@@ -11,8 +12,23 @@ password = 'Mytimy2shine'
 
 connection_engine = pyodbc.connect('Driver={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
-
-
 df = pd.read_csv("C:/Users/Rbhuiyan/Documents/My Resumes/SFL Scientific Interview/DATA.csv")
 
+#Data Transformation
+
+#Using the apply function to transform string to lower case
+df["gender"] = df["gender"].apply(str.lower)
+
+#Reorder columns
+df = df[['id', 'first_name', 'last_name', 'gender', 'email', 'ip_address']]
+
+#Extracting Company name using string manipulation
+s1 = df['email'].str.split("@", n=1, expand=True)
+
+s2 = s1[1].str.split(".",n=0,expand = True)
+
+df['company'] = s2[0]
+
 print(df.head())
+
+print(df.info())
